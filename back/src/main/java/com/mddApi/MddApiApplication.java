8257ba96e56b5f26d75244c2b86f95baefe727f7
Laura Vongsy <laura.vongsy@oppysoft.fr>
2025-03-16 21:06:09 +0100

    Update Spring and Java version

--- back/src/main/java/com/mddApi/MddApiApplication.java
+++ back/src/main/java/com/mddApi/MddApiApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.mddapi;
+package com.mddApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back/src/test/java/com/mddApi/MddApiApplicationTests.java
+++ back/src/test/java/com/mddApi/MddApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.mddapi;
+package com.mddApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
