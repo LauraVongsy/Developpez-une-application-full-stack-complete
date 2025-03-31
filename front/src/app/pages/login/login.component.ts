7cc8532f0ae68bf80cc4982c452bb1e8cfd662e8
Laura Vongsy <laura.vongsy@oppysoft.fr>
2025-03-31 15:44:00 +0200

    implementing the login fonctionality frontend

---
+++ front/src/app/pages/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterRequest } from 'src/app/interfaces/RegisterRequest.interface';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+public onError = false;
+  public form;
+
+  constructor(private authService: AuthService,
+              private fb: FormBuilder,
+              private router: Router) {
+    // Déplacer l'initialisation ici, car fb est maintenant défini
+    this.form = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40)
+        ]
+      ]
+    });
+  }
+
+  public submit(): void {
+    const registerRequest = this.form.value as RegisterRequest;
+    this.authService.login(registerRequest).subscribe({
+        next: () => this.router.navigate(['/home']),
+        error: () => this.onError = true,
+      }
+    );
+  }
+}
